453b8984770af5e3cb1e9db6fa8adc1fe99e33ae
liukatkat <katrinaliutinnam@gmail.com>
2022-07-04 17:51:31 +0800

    Initial Commit

---
+++ Tic Stack Toe/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Tic Stack Toe
+//
+//  Created by Katrina Liu on 7/4/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Tic Stack Toe/Tic_Stack_ToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tic_Stack_ToeApp.swift
+//  Tic Stack Toe
+//
+//  Created by Katrina Liu on 7/4/22.
+//
+
+import SwiftUI
+
+@main
+struct Tic_Stack_ToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
